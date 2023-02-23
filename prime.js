5d0e215141e51c5cb79e3f30954b6d8e4000bcb6
sai <saitejakarnati555@gmail.com>
2023-02-23 13:42:07 +0530

    new prime change file

---
+++ prime.js
@@ -0,0 +1,23 @@
+function isPrime(x) {
+
+    if (x == 1) {
+        return false
+    }
+    if (x == 2) {
+        return true
+    } else {
+        return false
+        for (var i = 2; i < x; i++) {
+            if (x % i == 0) {
+                return false
+            }
+        }
+    }
+}
+function add(n1, n2) {
+    return n1 + n2
+}
+let m = isPrime(53)
+let n = add(6, 7)
+console.log(n)
+console.log(m)
