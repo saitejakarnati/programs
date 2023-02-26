b23b599de49c3af5e0cd745975b7a28f6e181f71
Sai Teja Karnati <124346469+saitejakarnati@users.noreply.github.com>
2023-02-26 07:49:32 +0530

    Update array sort.js

--- Array sort/array sort.js
+++ Array sort/array sort.js
@@ -2,7 +2,6 @@ function sortarray(arr) {
     for(let i=0;i<arr.length-1;i++){
         if(arr[i]>arr[i+1]) {
             arr[i]=arr[i+1]
-            arr[i+1]=arr[i]
         }
     }
     return console.log(arr)
