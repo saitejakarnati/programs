3090029707bda1109458eda44eeed9f10c9f5b63
saitejakarnati <saitejakarnati555@gmail.com>
2023-04-01 00:30:31 +0530

    3table

--- three table/threeTable.js
+++ three table/threeTable.js
@@ -1,1 +1,10 @@
-m
+function print3table(){
+let x = 3;
+let y = "";
+for (let i = 1; i <= 10; i++) {
+    console.log(x + " * " + i + " = " + x * i )
+} 
+return y
+}
+let x= print3table();
+console.log(x)
